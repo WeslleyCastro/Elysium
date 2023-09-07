4c9f6a6308bf2068f35aee1e21a445dd1f846eb5
Weslley Castro <weslley.nado@gmail.com>
2023-09-07 18:18:59 -0300

    next auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export default function middleware(req: NextRequest) {
-  const token = req.cookies.get("next-auth.session-token")?.value || req.cookies.get("next-auth.session-token.0")
-  
-  const signInUrl = new URL("/signin", req.url) 
-
-  if (!token) {
-    if(req.nextUrl.pathname === "/signin"){
-      return NextResponse.next();
-    }
-    return NextResponse.redirect(signInUrl);
-  }
-}
+export { default } from "next-auth/middleware"
 
 export const config = {
-  matcher: ["/profile", "/share"]
+  matcher: ["/share"]
 }
